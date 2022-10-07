60336eba828ae730346c3a0aee89ad0eaa8bde5b
REDCROSS16 <belkill@mail.ru>
2022-10-07 22:13:26 +0300

    update

--- src/Controller/Products.php
+++ src/Controller/Products.php
@@ -7,6 +7,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class Products extends AbstractController
 {
+    #[Route('/', name: '', methods: ['GET'])]
+    public function lowerPrice()
+    {
+        
+    }
+    
     #[Route('/products/{id}/promotions', name: 'promotions', methods: ['GET'])]
     public function promotions()
     {
