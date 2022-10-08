377c11147063eabf370b6b4f6db40274c5358c3c
REDCROSS16 <belkill@mail.ru>
2022-10-08 23:19:18 +0300

    update

--- src/Controller/Products.php
+++ src/Controller/Products.php
@@ -3,18 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class Products extends AbstractController
 {
-    #[Route('/', name: '', methods: ['GET'])]
-    public function lowerPrice()
+    #[Route('/products/{id<\d>}/lowest-price', name: '', methods: ['POST'])]
+    public function lowestPrice(Request $request,int $id): Response
     {
-        
+        if ($request->headers->has(''))
+        return new Response(
+            [
+                'name' => 'alex',
+                'age' => 29,
+                'id' => $id
+            ], 200
+        );
     }
     
     #[Route('/products/{id}/promotions', name: 'promotions', methods: ['GET'])]
-    public function promotions()
+    public function promotions(): Response
     {
 
     }
