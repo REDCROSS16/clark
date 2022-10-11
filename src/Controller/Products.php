747d720aa65cf5ce174b59312e4d54672af09eb2
REDCROSS16 <belkill@mail.ru>
2022-10-11 23:49:05 +0300

    serializer + dto

--- src/Controller/Products.php
+++ src/Controller/Products.php
@@ -3,37 +3,43 @@
 namespace App\Controller;
 
 use App\Component\Tools\Procent;
+use App\DTO\LowestPriceEnquiry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class Products extends AbstractController
 {
-    public function __construct(private readonly Procent)
+    public function __construct()
     {
     }
 
     #[Route('/products/{id<\d>}/lowest-price', name: '', methods: ['POST'])]
-    public function lowestPrice(Request $request,int $id): Response
+    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): Response
     {
-        $data = '3.15';
-        $result = Procent::from($data);
 
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse(['error' => 'Promotions Engine failure message'],
+                $request->headers->get('force_fail')
+            );
+        }
 
-        if ($request->headers->has(''))
-        return new Response(
-            [
-                'name' => 'alex',
-                'age' => 29,
-                'id' => $id
-            ], 200
-        );
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+
+//        1. deserialize json data into enquireDTO
+//        2. pass the enquire into a promotions filter
+//        3. return the modified enquire
+
+        return new JsonResponse($result, 200);
     }
     
-    #[Route('/products/{id}/promotions', name: 'promotions', methods: ['GET'])]
+    #[Route('/products/{id<\d>}/promotions', name: 'promotions', methods: ['GET'])]
     public function promotions(): Response
     {
-
+        return new Response('');
     }
 }
