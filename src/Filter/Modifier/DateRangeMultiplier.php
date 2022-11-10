7b70984013776637686f785c1a35f78731f22490
REDCROSS16 <belkill@mail.ru>
2022-11-10 23:51:18 +0300

    update

--- src/Filter/Modifier/DateRangeMultiplier.php
+++ src/Filter/Modifier/DateRangeMultiplier.php
@@ -13,13 +13,9 @@ class DateRangeMultiplier implements  PriceModifierInterface
         $from = date_create($promotion->getCriteria()['from']);
         $to = date_create($promotion->getCriteria()['to']);
 
-        dd($promotion);
-
-
         if ($requestDate >= $from && $requestDate < $to ) {
             return $price * $quantity;
         }
-
 
         return ($price * $quantity) * $promotion->getAdjustment();
     }
