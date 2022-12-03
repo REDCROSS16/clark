b0567e573b68d77db5f8c9e7400e8a76560b5453
REDCROSS16 <belkill@mail.ru>
2022-12-03 23:42:32 +0300

    update

---
+++ src/Patterns/Creational/Builder/DesktopComputer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Patterns\Creational\Builder;
+
+class DesktopComputer
+{
+    private $components = [];
+
+    public function __construct()
+    {
+        array_push($this->components, 'some details');
+    }
+
+    public function getDescription(): string
+    {
+        $description = '';
+        if (isset($this->components[1])) {
+            foreach ($this->components as $component) {
+                $description .= $component . ' ';
+            }
+            return $description;
+        }
+
+        return $this->components[0];
+    }
+}
