f9845d58f62b892474c97db55e66bf13a2e302a5
REDCROSS16 <belkill@mail.ru>
2022-12-27 00:03:23 +0300

    decorator

--- src/Patterns/Structural/Decorator/realization.php
+++ src/Patterns/Structural/Decorator/realization.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App\Patterns\Structural\Decorator;
 
+require_once '../../../../vendor/autoload.php';
 
 function getNotification(string $text, NotifierInterface $decorator) {
-    echo 'MEssages' . PHP_EOL;
-    echo $decorator->notify($text);
+    echo 'Messages from: ';
+    echo $decorator->notify($text) .PHP_EOL;
 }
 
 $textNotification = new SimpleNotification();
